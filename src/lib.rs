150c9954ac96a5cf449ce905d1afb81e75100ff1
Akira Mizuno <akmizno@gmail.com>
2021-05-09 23:21:46 +0900

    Move crate attribute line

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,2 @@
-#![cfg_attr(all(windows, feature = "nightly"), feature(windows_by_handle))]
 pub mod entry;
 pub mod walk;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(all(windows, feature = "nightly"), feature(windows_by_handle))]
 fn main() {
     println!("Hello, world!");
 }
