f83496091d783e23e64b99e8337b995f124ac011
Akira Mizuno <akmizno@gmail.com>
2021-06-20 01:47:00 +0900

    Add LinkTo trait

--- src/entry/traits.rs
+++ src/entry/traits.rs
@@ -1,4 +1,5 @@
 use async_trait::async_trait;
+use futures::future::Future;
 use memmap::MmapOptions;
 use std::hash::Hasher;
 use std::io;
@@ -120,5 +121,219 @@ pub trait ContentEq: FileAttr {
         } else {
             tokio::task::block_in_place(move || eq_content_mmap(self_path, other_path))
         }
+    }
+}
+
+/// Create a link dst pointing to src.
+/// Both src and dst must be existing files.
+async fn link<P, F, Fut>(src: P, dst: P, f: F) -> io::Result<()>
+    where P: AsRef<Path>,
+          F: FnOnce(PathBuf, PathBuf) -> Fut,
+          Fut: Future<Output=io::Result<()>>,
+{
+    let src = src.as_ref();
+    let dst = dst.as_ref();
+
+    let abspath = tokio::fs::canonicalize(dst).await?;
+    let dir = abspath.parent().unwrap();
+    let filename = src.file_name().unwrap();
+
+    // Try to create a link.
+    // Make a temporary link before modifying the dst.
+    let tmpdir = tempfile::tempdir_in(dir)?;
+    let tmplink = tmpdir.path().join(filename);
+    let tmplink_result = f(src.to_owned(), tmplink.to_owned()).await;
+    match tmplink_result {
+        Ok(_) => {
+            log::info!("Create a temporary link {} to {}.", tmplink.display(), src.display());
+        },
+        Err(e) => {
+            log::error!("Fail to create a temporary link {} to {}.", tmplink.display(), src.display());
+            return Err(e);
+        },
+    };
+
+    // Try to rename the created link as dst.
+    let link_result = tokio::fs::rename(&tmplink, &dst).await;
+    match link_result {
+        Ok(_) => {
+            log::info!("Create a link {} to {}.", dst.display(), src.display());
+            Ok(())
+        },
+        Err(e) => {
+            log::error!("Fail to create a link {} to {}.", dst.display(), src.display());
+            Err(e)
+        }
+    }
+}
+async fn link_hard<P>(src: P, dst: P) -> io::Result<()>
+    where P: AsRef<Path>
+{
+    link(src, dst, tokio::fs::hard_link).await
+}
+async fn link_soft<P>(src: P, dst: P) -> io::Result<()>
+    where P: AsRef<Path>
+{
+    link(src, dst, tokio::fs::symlink).await
+}
+#[async_trait]
+pub trait LinkTo: FileAttr {
+    async fn into_hardlink(self, to: &Path) -> io::Result<()>
+        where Self: Sized + Sync
+    {
+        link_hard(to, self.path()).await
+    }
+    async fn into_symlink(self, to: &Path) -> io::Result<()>
+        where Self: Sized + Sync
+    {
+        link_soft(to, self.path()).await
+    }
+}
+
+#[cfg(unix)]
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::Permissions;
+    use std::os::unix::fs::{MetadataExt, PermissionsExt};
+    use test_env_log::test;
+
+    fn make_tempdir() -> (tempfile::TempDir, PathBuf, PathBuf) {
+        let dir = tempfile::tempdir().unwrap();
+        let f1 = dir.path().join("f1");
+        let f2 = dir.path().join("f2");
+        std::fs::File::create(f1.as_path()).unwrap();
+        std::fs::File::create(f2.as_path()).unwrap();
+        (dir, f1, f2)
+    }
+
+    #[test(tokio::test)]
+    async fn link_hard_ok() {
+        let (_tmpdir, src, dst) = make_tempdir();
+        let src_meta = std::fs::symlink_metadata(&src).unwrap();
+        let src_type = src_meta.file_type();
+        let dst_meta = std::fs::symlink_metadata(&dst).unwrap();
+        let dst_type = dst_meta.file_type();
+        assert!(src_type.is_file() && dst.exists());
+        assert!(dst_type.is_file() && dst.exists());
+        assert_eq!(src_meta.dev(), dst_meta.dev());
+        assert_ne!(src_meta.ino(), dst_meta.ino());
+
+        link_hard(&src, &dst).await.unwrap();
+
+        let src_meta = std::fs::metadata(&src).unwrap();
+        let src_type = src_meta.file_type();
+        let dst_meta = std::fs::symlink_metadata(&dst).unwrap();
+        let dst_type = dst_meta.file_type();
+        assert!(src_type.is_file() && dst.exists());
+        assert!(dst_type.is_file() && dst.exists());
+        assert_eq!(src_meta.dev(), dst_meta.dev());
+        assert_eq!(src_meta.ino(), dst_meta.ino());
+    }
+    #[test(tokio::test)]
+    async fn link_hard_nonexistent_src() {
+        let (_tmpdir, src, dst) = make_tempdir();
+        std::fs::remove_file(&src).unwrap();
+
+        assert!(!src.exists());
+        assert!(dst.is_file() && dst.exists());
+
+        let result = link_hard(&src, &dst).await;
+
+        assert!(result.is_err());
+    }
+    #[test(tokio::test)]
+    async fn link_hard_nonexistent_dst() {
+        let (_tmpdir, src, dst) = make_tempdir();
+        std::fs::remove_file(&dst).unwrap();
+
+        assert!(src.is_file() && src.exists());
+        assert!(!dst.exists());
+
+        let result = link_hard(&src, &dst).await;
+
+        assert!(result.is_err());
+    }
+    #[test(tokio::test)]
+    async fn link_hard_permissions_err() {
+        let (tmpdir, src, dst) = make_tempdir();
+        assert!(src.is_file() && src.exists());
+        assert!(dst.is_file() && dst.exists());
+
+        let perm = Permissions::from_mode(0o555);
+        std::fs::set_permissions(tmpdir.path(), perm).unwrap();
+        let result = link_hard(&src, &dst).await;
+
+        assert!(result.is_err());
+
+        let perm = Permissions::from_mode(0o755);
+        std::fs::set_permissions(&tmpdir.path(), perm).unwrap();
+    }
+
+    #[test(tokio::test)]
+    async fn link_soft_ok() {
+        let (_tmpdir, src, dst) = make_tempdir();
+        let src_meta = std::fs::metadata(&src).unwrap();
+        let src_type = src_meta.file_type();
+        let dst_meta = std::fs::symlink_metadata(&dst).unwrap();
+        let dst_type = dst_meta.file_type();
+        assert!(src_type.is_file() && dst.exists());
+        assert!(dst_type.is_file() && dst.exists());
+        assert_eq!(src_meta.dev(), dst_meta.dev());
+        assert_ne!(src_meta.ino(), dst_meta.ino());
+
+        link_soft(&src, &dst).await.unwrap();
+
+        let src_meta = std::fs::metadata(&src).unwrap();
+        let src_type = src_meta.file_type();
+        let dst_meta = std::fs::symlink_metadata(&dst).unwrap();
+        let dst_meta2 = std::fs::metadata(&dst).unwrap();
+        let dst_type = dst_meta.file_type();
+        assert!(src_type.is_file() && dst.exists());
+        assert!(dst_type.is_symlink() && dst.exists());
+        assert_eq!(src_meta.dev(), dst_meta.dev());
+        assert_eq!(src_meta.ino(), dst_meta2.ino());
+    }
+    #[test(tokio::test)]
+    async fn link_soft_nonexistent_src() {
+        let (_tmpdir, src, dst) = make_tempdir();
+        std::fs::remove_file(&src).unwrap();
+
+        assert!(!src.exists());
+        assert!(dst.is_file() && dst.exists());
+
+        link_soft(&src, &dst).await.unwrap();
+
+        assert!(!src.exists());
+        let dst_meta = std::fs::symlink_metadata(&dst).unwrap();
+        let dst_type = dst_meta.file_type();
+        assert!(dst_type.is_symlink());
+    }
+    #[test(tokio::test)]
+    async fn link_soft_nonexistent_dst() {
+        let (_tmpdir, src, dst) = make_tempdir();
+        std::fs::remove_file(&dst).unwrap();
+
+        assert!(src.is_file() && src.exists());
+        assert!(!dst.exists());
+
+        let result = link_soft(&src, &dst).await;
+
+        assert!(result.is_err());
+    }
+    #[test(tokio::test)]
+    async fn link_soft_permissions_err() {
+        let (tmpdir, src, dst) = make_tempdir();
+        assert!(src.is_file() && src.exists());
+        assert!(dst.is_file() && dst.exists());
+
+        let perm = Permissions::from_mode(0o555);
+        std::fs::set_permissions(tmpdir.path(), perm).unwrap();
+        let result = link_soft(&src, &dst).await;
+
+        assert!(result.is_err());
+
+        let perm = Permissions::from_mode(0o755);
+        std::fs::set_permissions(&tmpdir.path(), perm).unwrap();
     }
 }
